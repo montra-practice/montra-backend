a937596a5fa82986f7fb568c2100c639c7462640
Ricky_Liu <ricky_liu@epam.com>
2022-06-16 15:21:00 +0800

    Create Montra project

---
+++ src/main/java/com/epam/Montra/MontraApplication.java
@@ -0,0 +1,14 @@
+package com.epam.Montra;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class MontraApplication {
+
+	public static void main(String[] args) {
+		run(MontraApplication.class, args);
+	}
+
+}
